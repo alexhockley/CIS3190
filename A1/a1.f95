b43ca9c6f4dd3db32a81b5ec0fdfd250ef554e36
Alex Hockley <lookitsmyvideo@gmail.com>
2016-02-03 14:37:00 -0500

    Finally got file reading working into matrix

--- A1/a1.f95
+++ A1/a1.f95
@@ -7,9 +7,10 @@ type(stack_type) :: location_stack
 
 integer :: rows = 0
 integer :: columns = 0
-integer :: i = 0;
-integer :: j = 0;
-character(len=200) :: temp_val
+integer :: i = 0
+integer :: j = 0
+integer :: linelen = 0
+character(len=1000) :: temp_val
 character(len=200) :: filename
 character (len=1), dimension(1000,1000) :: maze_matrix
 
@@ -22,12 +23,14 @@ print*, rows
 print*, columns
 
 
-
 do i=1, rows
-  do j=1, columns
-    read(2,'(A1)') maze_matrix(i,j)
+  read(2,*) temp_val
+  linelen = len(trim(temp_val))
+  do j=1, linelen
+    maze_matrix(i,j) = temp_val(j:j)
   end do
 end do
+
 
 close(2)
 end program a1
