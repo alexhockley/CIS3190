b5ef1f5e8afe84cdb3a566e6875a0f4ffcaa238b
Alex Hockley <lookitsmyvideo@gmail.com>
2016-03-02 13:29:03 -0500

    Added string size

--- A2/a2.adb
+++ A2/a2.adb
@@ -2,18 +2,14 @@ with Ada.Text_IO;
 use Ada;
 
 procedure A2 is
-  File_Name       : String;
+  File_Name       : String(1..100);
 
   Prompt_One      : constant String := "Please enter the name of the file to read";
 
   File            : Text_IO.File_Type;
   Char_Temp       : Character;
 
-  Rows : constant Integer := 9;
-  Cols : constant Integer := 9;
-  subtype Row_Range is Integer range 1..Rows;
-  subtype Col_Range is Integer range 1..Cols;
-  type Puzzle_Type is array (Row_Range,Col_Range) of Integer;
+  type Puzzle_Type is array (1..9,1..9) of Integer;
 
   Puzzle : Puzzle_Type;
 
