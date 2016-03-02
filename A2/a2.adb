6dab20869c2c290cf3788b5f21be0b19448f79af
Alex Hockley <lookitsmyvideo@gmail.com>
2016-03-02 13:54:33 -0500

    Added trimming of filename

--- A2/a2.adb
+++ A2/a2.adb
@@ -1,11 +1,10 @@
-with Ada.Text_IO;
+with Ada.Text_IO, Ada.Integer_IO;
 use Ada;
 
 procedure A2 is
   File_Name       : String(1..100) := (others => ' ');
-  Length          : Integer;
+  Length          : Integer range 0..100;
 
-  Prompt_One      : constant String := "Please enter the name of the file to read: ";
 
   File            : Text_IO.File_Type;
   Char_Temp       : Character;
@@ -16,19 +15,20 @@ procedure A2 is
 
 begin
 
-  Text_IO.Put(Prompt_One);
+  Text_IO.Put("Please enter the name of the file to read: ");
   Text_IO.Get_Line(File_Name, Length);
+
 
   Text_IO.Put("Filename: " & File_Name);
 
-  Text_IO.Open (File=>File, Mode=>Text_IO.In_File, Name=>File_Name);
+  Text_IO.Open (File=>File, Mode=>Text_IO.In_File, Name=>File_Name (1..Length));
 
   while not Text_IO.End_Of_File (File) loop
        while not Text_IO.End_Of_Line (File) loop
         Text_IO.Get (File => File, Item => Char);
         Text_IO.Put (Char);
        end loop;
-       Text_IO.Skip_Line (File);  
+       Text_IO.Skip_Line (File);
        Text_IO.New_Line;
    end loop;
 
