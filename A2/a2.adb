7da2235279207f8ec7749a1d5e06a12f26ce9da6
Alex Hockley <lookitsmyvideo@gmail.com>
2016-03-02 13:31:49 -0500

    Working on printing entered filename

--- A2/a2.adb
+++ A2/a2.adb
@@ -4,7 +4,7 @@ use Ada;
 procedure A2 is
   File_Name       : String(1..100);
 
-  Prompt_One      : constant String := "Please enter the name of the file to read";
+  Prompt_One      : constant String := "Please enter the name of the file to read: ";
 
   File            : Text_IO.File_Type;
   Char_Temp       : Character;
@@ -18,6 +18,6 @@ begin
   Text_IO.Put(Prompt_One);
   Text_IO.Get(File_Name);
 
-  Text_IO.Put(File_Name);
+  Text_IO.Put("Filename: " & File_Name);
 
 end A2;
