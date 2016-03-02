157d1f740659a9e54c0bae0b1fe5bb361cf03aad
Alex Hockley <lookitsmyvideo@gmail.com>
2016-03-02 13:22:02 -0500

    Added range vars

--- A2/a2.adb
+++ A2/a2.adb
@@ -2,13 +2,18 @@ with Ada.Text_IO;
 use Ada;
 
 procedure A2 is
-  File_Name : String;
+  File_Name       : String;
 
-  Prompt_One : constant String := "Please enter the name of the file to read\n";
+  Prompt_One      : constant String := "Please enter the name of the file to read";
 
-  File      : Text_IO.File_Type;
-  Char      : Character;
-  type Puzzle is array (9,9) of Integer;
+  File            : Text_IO.File_Type;
+  Char_Temp       : Character;
+
+  Rows : constant Integer := 9;
+  Cols : constant Integer := 9;
+  subtype Row_Range is Integer range 1..Rows;
+  subtype Col_Range is Integer range 1..Cols;
+  type Puzzle is array (Row_Range,Col_Range) of Integer;
 
 begin
 
