b39c594f2c8e4eb9a40c8088ba354c0becc139be
Alex Hockley <ahockley@granger.socs.uoguelph.ca>
2016-03-02 14:19:31 -0500

    Reading file into integer array

--- A2/a2.adb
+++ A2/a2.adb
@@ -5,6 +5,8 @@ procedure A2 is
   File_Name       : String(1..100);
   Length          : Integer range 0..100;
 
+  Row_Counter	  : Integer := 1;
+  Col_Counter     : Integer := 1;
 
   File            : Text_IO.File_Type;
   Char_Temp       : Character;
@@ -27,6 +29,16 @@ begin
   While not Text_IO.End_Of_File (File) loop
     Text_IO.Get(File=>File, Item=>Char_Temp);
     Text_IO.Put(Char_Temp);
+    
+    if Col_Counter <= 9 then
+    Puzzle(Row_Counter,Col_Counter) := Character'Pos(Char_Temp);
+    end if;
+    
+    Row_Counter := Row_Counter + 1;
+    if Row_Counter = 10 then
+       Col_Counter := Col_Counter + 1;
+       Row_Counter := 1;   
+    end if;
   end loop;
   Text_IO.New_Line;
 
