c0986479f9c11e9cd6ccc7008520bc978e1ba755
Alex Hockley <ahockley@granger.socs.uoguelph.ca>
2016-03-02 14:05:46 -0500

    File opning finished

--- A2/a2.adb
+++ A2/a2.adb
@@ -1,8 +1,8 @@
-with Ada.Text_IO
+with Ada.Text_IO, Ada.Integer_Text_IO;
 use Ada;
 
 procedure A2 is
-  File_Name       : String(1..100) := (others => ' ');
+  File_Name       : String(1..100);
   Length          : Integer range 0..100;
 
 
@@ -16,20 +16,12 @@ procedure A2 is
 begin
 
   Text_IO.Put("Please enter the name of the file to read: ");
-  Text_IO.Get_Line(File_Name, Length);
+  Text_IO.Get_Line(Item=>File_Name, Last=>Length);
 
 
-  Text_IO.Put("Filename: " & File_Name);
+  Text_IO.Put("Filename: " & File_Name(1..Length));
+  Integer_Text_IO.Put(Length);
 
   Text_IO.Open (File=>File, Mode=>Text_IO.In_File, Name=>File_Name (1..Length));
-
-  while not Text_IO.End_Of_File (File) loop
-       while not Text_IO.End_Of_Line (File) loop
-        Text_IO.Get (File => File, Item => Char_Temp);
-        Text_IO.Put (Char_Temp);
-       end loop;
-       Text_IO.Skip_Line (File);
-       Text_IO.New_Line;
-   end loop;
 
 end A2;
