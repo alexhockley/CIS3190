208fa9cfdc7ec4414d04007e2bae424bba799051
Alex Hockley <lookitsmyvideo@gmail.com>
2016-03-22 13:43:25 -0400

    added encryption

--- A3/encrypt.cob
+++ A3/encrypt.cob
@@ -11,4 +11,20 @@ linkage section.
 
 procedure division using str, encrypted-str.
  move str to encrypted-str.
+ PERFORM VARYING i FROM 1 BY 1 UNTIL i > FUNCTION LENGTH(str)
+        IF encrypted-str (i:1) IS NOT ALPHABETIC OR encrypted-str (i:1) = SPACE
+            EXIT PERFORM CYCLE
+        END-IF
+
+        IF encrypted-str (i:1) IS ALPHABETIC-UPPER
+            MOVE FUNCTION ORD("A") TO a
+        ELSE
+            MOVE FUNCTION ORD("a") TO a
+        END-IF
+
+        MOVE FUNCTION CHAR(FUNCTION MOD(FUNCTION ORD(encrypted-str (i:1))
+                - a + i, 26) + a)
+            TO encrypted-str (i:1)
+    END-PERFORM
+    .
  goback.
