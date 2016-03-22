2fca3269fe391490edf7fd808ca599589cf2174e
Alex Hockley <lookitsmyvideo@gmail.com>
2016-03-22 13:41:04 -0400

    Variable typo

--- A3/decrypt.cob
+++ A3/decrypt.cob
@@ -7,7 +7,7 @@ working-storage section.
 
 linkage section.
 77 str pic X(50).
-77 encrypted-str pic X(50).
+77 decrypted-str pic X(50).
 
 procedure division using str, decrypted-str.
  move str to decrypted-str.
