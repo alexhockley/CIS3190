7264b9086ab4dfafb814397f1386feaec73a917a
Alex Hockley <lookitsmyvideo@gmail.com>
2016-03-22 13:38:37 -0400

    readded my code

--- A3/encrypt.cob
+++ A3/encrypt.cob
@@ -6,10 +6,9 @@ data division.
 working-storage section.
 
 linkage section.
-77 g1 pic 99.
-77 g2 pic 99.
-77 res pic 9(3).
+77 str pic X(50).
+77 encrypted-str pic X(50).
 
-procedure division using g1, g2, res.
- compute res = g1 + g2.
+procedure division using str, encrypted-str.
+ move str to encrypted-str.
  goback.
