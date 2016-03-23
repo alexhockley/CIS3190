efe9d1728b53519824fd559fbf8d4dc998a64be2
Alex Hockley <lookitsmyvideo@gmail.com>
2016-03-23 13:47:37 -0400

    Actually added the files this time

---
+++ A3/fileread.cob
@@ -0,0 +1,16 @@
+identification division.
+program-id. fileread.
+environment division.
+
+data division.
+working-storage section.
+
+linkage section.
+77 str      pic X(50000).
+77 filename pic X(50).
+
+procedure division using str, filename.
+  display 'reading file'
+  open input filename.
+  read filename into str.
+goback.
